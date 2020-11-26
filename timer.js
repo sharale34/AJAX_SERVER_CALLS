cd2ed3640bd192d65e4d6f7f96aa04eb224b530d
sharale34 <sourabhharale8000@gmail.com>
2020-11-26 13:10:32 +0530

    Added: Ability to demonstrate Async nature of JavaScript

--- timer.js
+++ timer.js
@@ -1,1 +1,12 @@
-// Ajax - Async, Callback && Promise
+function showTime() {
+    const date = new Date();
+    return date.getHours() + " Hrs : " + date.getMinutes() + " Minutes : " + date.getSeconds() + " Seconds : ";
+}
+
+function showSessionExpire() {
+    console.log("Activity-B : Your session expired at " + showTime());
+}
+
+console.log("Activity-A : Triggering Activity-B at " + showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity-A : Triggering Activity-B at " + showTime() + " will execute after 5 seconds");
